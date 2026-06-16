776a4ee6a54039f909d6feded3c591790c3bfa19
ChristianRenzLedesma <renzledesma76@gmail.com>
2026-06-17 00:24:57 +0800

    Add render.yaml, package.json, server.js for Render web service deployment

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const MIME = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+};
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      return res.end('<h1>404 Not Found</h1>');
+    }
+    res.writeHead(200, { 'Content-Type': MIME[ext] || 'text/plain' });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
